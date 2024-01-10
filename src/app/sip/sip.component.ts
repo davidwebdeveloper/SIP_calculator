d556145155c2d92cbffab8dae99015fa11e5f427
@PravinabdulkalamMathikannan <pravinabdulkalam.m@finstein.ai>
2024-01-10 11:22:17 +0530

    chart done

--- src/app/sip/sip.component.ts
+++ src/app/sip/sip.component.ts
@@ -16,7 +16,35 @@ export class SipComponent {
     return (this.principal*this.time*12)
   }
 
-  // calculateCompoundInterest(): number {
-  //   return this.principal * Math.pow(1 + this.rate / 100, this.time) - this.principal;
-  // }
+  data: any;
+
+  options: any;
+
+  ngOnInit() {
+      const documentStyle = getComputedStyle(document.documentElement);
+      const textColor = documentStyle.getPropertyValue('--text-color');
+
+      this.data = {
+          labels: ['A', 'B', 'C'],
+          datasets: [
+              {
+                  data: [300, 50, 100],
+                  backgroundColor: [documentStyle.getPropertyValue('--blue-500'), documentStyle.getPropertyValue('--yellow-500'), documentStyle.getPropertyValue('--green-500')],
+                  hoverBackgroundColor: [documentStyle.getPropertyValue('--blue-400'), documentStyle.getPropertyValue('--yellow-400'), documentStyle.getPropertyValue('--green-400')]
+              }
+          ]
+      };
+
+
+      this.options = {
+          cutout: '60%',
+          plugins: {
+              legend: {
+                  labels: {
+                      color: textColor
+                  }
+              }
+          }
+      };
+  }
 }
