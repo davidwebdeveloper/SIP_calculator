aa16f85754ec7b2de48cc3f572183098ae02cad0
davidwebdeveloper <mdd11024@gmail.com>
2024-01-10 13:17:24 +0530

    chnge

--- src/app/sip/sip.component.ts
+++ src/app/sip/sip.component.ts
@@ -12,8 +12,8 @@ export class SipComponent {
   calculateSimpleInterest(): number {
     return (this.principal * this.rate * this.time) / 100;
   }
-  calculatetotalinvestment(){
-    return (this.principal*this.time*12)
+  calculatetotalinvestment() {
+    return (this.principal * this.time * 12)
   }
 
   data: any;
@@ -21,30 +21,32 @@ export class SipComponent {
   options: any;
 
   ngOnInit() {
-      const documentStyle = getComputedStyle(document.documentElement);
-      const textColor = documentStyle.getPropertyValue('--text-color');
+    const documentStyle = getComputedStyle(document.documentElement);
+    const textColor = documentStyle.getPropertyValue('--text-color');
 
-      this.data = {
-          labels: ['A', 'B', 'C'],
-          datasets: [
-              {
-                  data: [300, 50, 100],
-                  backgroundColor: [documentStyle.getPropertyValue('--blue-500'), documentStyle.getPropertyValue('--yellow-500'), documentStyle.getPropertyValue('--green-500')],
-                  hoverBackgroundColor: [documentStyle.getPropertyValue('--blue-400'), documentStyle.getPropertyValue('--yellow-400'), documentStyle.getPropertyValue('--green-400')]
-              }
-          ]
-      };
+    this.data = {
+      labels: ['A', 'B', 'C'],
+      datasets: [
+        {
+          data: [300, 50, 100],
+          backgroundColor: [documentStyle.getPropertyValue('--blue-500'), documentStyle.getPropertyValue('--yellow-500'), documentStyle.getPropertyValue('--green-500')],
+          hoverBackgroundColor: [documentStyle.getPropertyValue('--blue-400'), documentStyle.getPropertyValue('--yellow-400'), documentStyle.getPropertyValue('--green-400')]
+        }
+      ]
+    };
 
 
-      this.options = {
-          cutout: '60%',
-          plugins: {
-              legend: {
-                  labels: {
-                      color: textColor
-                  }
-              }
+    this.options = {
+      cutout: '60%',
+      plugins: {
+        legend: {
+          labels: {
+            color: textColor
           }
-      };
+        }
+      }
+    };
   }
+
+
 }
