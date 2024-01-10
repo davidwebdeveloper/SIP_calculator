78a14931b474c6b6ddb6a419536395142b834910
@PravinabdulkalamMathikannan <pravinabdulkalam.m@finstein.ai>
2024-01-10 11:17:05 +0530

    calculation done

--- src/app/sip/sip.component.ts
+++ src/app/sip/sip.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./sip.component.scss']
 })
 export class SipComponent {
+  principal: number = 0;
+  rate: number = 0;
+  time: number = 0;
+  calculateSimpleInterest(): number {
+    return (this.principal * this.rate * this.time) / 100;
+  }
+  calculatetotalinvestment(){
+    return (this.principal*this.time*12)
+  }
 
+  // calculateCompoundInterest(): number {
+  //   return this.principal * Math.pow(1 + this.rate / 100, this.time) - this.principal;
+  // }
 }
