49fbc46c0407f4a71e048aeb232fd8c3ec92dd79
davidwebdeveloper <mdd11024@gmail.com>
2024-01-10 16:05:33 +0530

    ui changed

--- src/app/sip/sip.component.ts
+++ src/app/sip/sip.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import * as d3 from 'd3';
-import {ElementRef, ViewChild, AfterViewInit } from '@angular/core';
+import { ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-sip',
@@ -38,84 +38,96 @@ export class SipComponent {
     return this.investment
   }
 
-@ViewChild('doughnutChart', { static: true }) private chartContainer!: ElementRef;
+  formatCurrency(value: number): string {
+    return value.toLocaleString('en-IN', { style: 'currency', currency: 'INR' });
+  }
 
-radius!:number
-private chart: any;
+  @ViewChild('doughnutChart', { static: true }) private chartContainer!: ElementRef;
+
+  radius!: number
+  private chart: any;
 
 
-ngAfterViewInit() {
-  this.createDoughnutChart();
+  ngAfterViewInit() {
+    this.createDoughnutChart();
+  }
+
+  private createDoughnutChart() {
+    const width = 300;
+    const height = 300;
+    this.radius = Math.min(width, height) / 2;
+    this.time = 1
+    this.chart = d3.select(this.chartContainer.nativeElement).append('svg')
+      .attr('width', width)
+      .attr('height', height)
+      .append('g')
+      .attr('transform', `translate(${width / 2},${height / 2})`);
+
+    this.updateChart();
+  }
+
+  private updateChart() {
+    const data = [this.principal * this.time * 12, (this.principal * this.rate * this.time) / 100];
+
+    const color = d3.scaleOrdinal(['#EEF0FF', '#5367FF']);
+
+    const pie = d3.pie();
+
+    const arc = d3.arc()
+      .innerRadius(this.radius - 70)
+      .outerRadius(this.radius);
+
+
+
+
+    this.chart.selectAll('*').remove(); // Clear existing elements
+
+    const arcs = this.chart.selectAll('.arc')
+      .data(pie(data))
+      .enter()
+      .append('g')
+      .attr('class', 'arc');
+
+    arcs.append('path')
+      .attr('d', arc)
+      .attr('fill', (d: any, i: number) => color(String(i)));
+
+    arcs.append('text')
+      .attr('transform', (d: any) => 'translate(' + arc.centroid(d) + ')')
+      .attr('dy', '0.35em')
+      .text((d: any) => this.formatCurrency(d.data));
+
+
+  }
+
+  calculatepercentage() {
+    this.updateChart();
+  }
 }
 
-private createDoughnutChart() {
-  const width = 400;
-  const height = 400;
-  this.radius = Math.min(width, height) / 2;
-this.time=1
-  this.chart = d3.select(this.chartContainer.nativeElement).append('svg')
-    .attr('width', width)
-    .attr('height', height)
-    .append('g')
-    .attr('transform', `translate(${width / 2},${height / 2})`);
 
-  this.updateChart();
-}
 
-private updateChart() {
-  const data = [this.principal, (this.principal * this.rate* this.time) / 100];
 
-  const color = d3.scaleOrdinal(['#3498db', '#e74c3c']);
+// this.chart.append('text')
+//   .attr('x', 0)
+//   .attr('y', 0)
+//   .attr('text-anchor', 'middle')
+//   .text('Investment');
 
-  const pie = d3.pie();
+// this.chart.append('text')
+//   .attr('x', 0)
+//   .attr('y', 20)
+//   .attr('text-anchor', 'middle')
+//   .text(this.principal);
 
-  const arc = d3.arc()
-    .innerRadius(this.radius - 100)
-    .outerRadius(this.radius);
+// this.chart.append('text')
+//   .attr('x', 0)
+//   .attr('y', 50)
+//   .attr('text-anchor', 'middle')
+//   .text('Interest Rate');
 
-  this.chart.selectAll('*').remove(); // Clear existing elements
-
-  const arcs = this.chart.selectAll('.arc')
-    .data(pie(data))
-    .enter()
-    .append('g')
-    .attr('class', 'arc');
-
-  arcs.append('path')
-    .attr('d', arc)
-    .attr('fill', (d: any, i: number) => color(String(i)));
-
-  arcs.append('text')
-    .attr('transform', (d: any) => 'translate(' + arc.centroid(d) + ')')
-    .attr('dy', '0.35em')
-    .text((d: any) => d.data);
-
-  this.chart.append('text')
-    .attr('x', 0)
-    .attr('y', 0)
-    .attr('text-anchor', 'middle')
-    .text('Investment');
-
-  this.chart.append('text')
-    .attr('x', 0)
-    .attr('y', 20)
-    .attr('text-anchor', 'middle')
-    .text(this.principal);
-
-  this.chart.append('text')
-    .attr('x', 0)
-    .attr('y', 60)
-    .attr('text-anchor', 'middle')
-    .text('Interest Rate');
-
-  this.chart.append('text')
-    .attr('x', 0)
-    .attr('y', 80)
-    .attr('text-anchor', 'middle')
-    .text(this.rate + '%');
-}
-
-calculatepercentage() {
-  this.updateChart();
-}
-}
+// this.chart.append('text')
+//   .attr('x', 0)
+//   .attr('y', 70)
+//   .attr('text-anchor', 'middle')
+//   .text(this.rate + '%');
